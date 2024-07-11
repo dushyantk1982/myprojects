931b99a39e84b9191e99a5e0d420c70e7ac2c29d
dushyantk1982 <dushyantk.upadhyay82@gmail.com>
2024-07-11 12:11:09 +0530

    Add files via upload

---
+++ collegeportal/gallery/campus/campus.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>BVM Group of Colleges</title>
+<link rel="shortcut icon" href="Images/logo.jpg" />
+<!--<link href="stylesheet.css" type="text/css" rel="stylesheet">-->
+<link href="homestyle.css" type="text/css" rel="stylesheet">
+<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+
+    <style type="text/css">
+<!--
+.style8 {
+	font-size: 18px;
+	color: #003333;
+}
+-->
+    </style>
+</head>
+
+<body>
+<div align="center" class="style8">Campus Tour</div>
+<!-- Start WOWSlider.com BODY section -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+<li><img src="data1/images/1.jpg" alt="1" title="1" id="wows1_0"/></li>
+<li><img src="data1/images/2.jpg" alt="2" title="2" id="wows1_1"/></li>
+<li><img src="data1/images/3.jpg" alt="3" title="3" id="wows1_2"/></li>
+<li><img src="data1/images/4.jpg" alt="4" title="4" id="wows1_3"/></li>
+<li><img src="data1/images/5.jpg" alt="5" title="5" id="wows1_4"/></li>
+<li><img src="data1/images/6.jpg" alt="6" title="6" id="wows1_5"/></li>
+<li><img src="data1/images/7.jpg" alt="7" title="7" id="wows1_6"/></li>
+<li><img src="data1/images/8.jpg" alt="8" title="8" id="wows1_7"/></li>
+<li><img src="data1/images/9.jpg" alt="9" title="9" id="wows1_8"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="1"><img src="data1/tooltips/1.jpg" alt="1"/>1</a>
+<a href="#" title="2"><img src="data1/tooltips/2.jpg" alt="2"/>2</a>
+<a href="#" title="3"><img src="data1/tooltips/3.jpg" alt="3"/>3</a>
+<a href="#" title="4"><img src="data1/tooltips/4.jpg" alt="4"/>4</a>
+<a href="#" title="5"><img src="data1/tooltips/5.jpg" alt="5"/>5</a>
+<a href="#" title="6"><img src="data1/tooltips/6.jpg" alt="6"/>6</a>
+<a href="#" title="7"><img src="data1/tooltips/7.jpg" alt="7"/>7</a>
+<a href="#" title="8"><img src="data1/tooltips/8.jpg" alt="8"/>8</a>
+<a href="#" title="9"><img src="data1/tooltips/9.jpg" alt="9"/>9</a>
+</div></div>
+<span class="wsl"><a href="http://wowslider.com">Flash Slideshow</a> by WOWSlider.com v3.8</span>
+	<div class="ws_shadow"></div>
+	</div>
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+</body>
+</html>
