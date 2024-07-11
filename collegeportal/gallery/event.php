176cf862ab781aeedce7d616aefe4a1bb46e547e
dushyantk1982 <dushyantk.upadhyay82@gmail.com>
2024-07-11 12:09:31 +0530

    Add files via upload

---
+++ collegeportal/gallery/event.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>BVM Group of Colleges</title>
+<link rel="shortcut icon" href="Images/logo.jpg" />
+<!--<link href="stylesheet.css" type="text/css" rel="stylesheet">-->
+<link href="homestyle.css" type="text/css" rel="stylesheet">
+<style type="text/css">
+<!--
+.style8 {
+	color: #003333;
+	font-size: 24px;
+}
+-->
+</style>
+
+</head>
+
+<body>
+<table align="center">
+<tr><td width="98%">
+<p align="center" class="pagefont style8"><u>Events</u></p>
+<table width="100%" align="center" style="border:#003333;">
+<tr><td align="center">The Page is Under Construction.....</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ collegeportal/gallery/harmony.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>BVM Group of Colleges</title>
+<link rel="shortcut icon" href="Images/logo.jpg" />
+<!--<link href="stylesheet.css" type="text/css" rel="stylesheet">-->
+<link href="homestyle.css" type="text/css" rel="stylesheet">
+<!-- Start WOWSlider.com HEAD section -->
+	<!--<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>-->
+	<!-- End WOWSlider.com HEAD section -->
+
+    <style type="text/css">
+<!--
+.style8 {
+	font-size: 18px;
+	color: #003333;
+}
+-->
+    </style>
+</head>
+
+<body>
+<div align="center" class="style8">Harmony 2014</div>
+<div align="center"><img src="har.gif" height="500" width="600" />	</div>
+
+</body>
+</html>
